1f144046d6b6f19d072a72adc5a537b3a10a484e
Natália Lacca <nathy.lacca@gmail.com>
2022-11-01 09:48:31 -0300

    conceitos php

--- condicional.php
+++ condicional.php
@@ -1,5 +1,5 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 
 <head>
     <meta charset="UTF-8">
@@ -11,33 +11,30 @@
 <body>
 
     <h1>Estruturas condicionais</h1>
-    
 
     <?php
-    // Um jeito de escrever condicional
 
-    $teste = 'Joca';
+    $teste = 'Ana';
 
     if ($teste == 'Maria') {
         // Se a exp acima for true
-        echo 'Usuário correto!';
-        echo 'Bem-vindo!';
+        echo 'Ola           ' . $teste;
+        echo 'Bem-vinda!';
     } elseif ($teste == 'Joca') {
-        // Se a primeira exp for false e a segunda for true
-        echo 'Olá Joca!';
-        echo 'Como vai?';
+        // Se a primeira exp for false a acima for true
+        echo 'Ola ' . $teste;
+        echo 'Como vai!';
+    } elseif ($teste == 'Ana') {
+        echo 'Ola ' . $teste;
+        echo 'Tudo ok?';
     } else {
         // Se todas as exp forem falsas
         echo 'Usuário incorreto!';
         echo 'Tente novamente!';
     }
-
     ?>
-
     <hr>
-
     <?php
-    // Outro jeito de escrever condicional -> melhor maneira
 
     $teste = 'Ana';
 
@@ -46,11 +43,10 @@
         echo 'Ola           ' . $teste;
         echo 'Bem-vinda!';
     elseif ($teste == 'Joca') :
-        // Se a primeira exp for false e a segunda for true
+        // Se a primeira exp for false a acima for true
         echo 'Ola ' . $teste;
         echo 'Como vai!';
     elseif ($teste == 'Ana') :
-        // Se a primeira exp e a segunda for false
         echo 'Ola ' . $teste;
         echo 'Tudo ok?';
     else :
@@ -60,31 +56,58 @@
     endif;
 
     ?>
-
     <hr>
-    
     <?php
-    // Escrevendo switch condicional -> a maneira mais simples/básica de comparação
 
-    $teste = 'Maria';
+    $teste = 'Anna';
 
-    switch($teste) :
+    switch ($teste) {
+
         case 'Maria':
             echo 'Ola ' . $teste;
             echo 'Bem-vinda!';
-            break; // Serve para parar o teste caso acima seja true
+            break;
         case 'Joca':
             echo 'Ola ' . $teste;
             echo 'Como vai!';
-            break; // Serve para parar o teste caso acima seja true
+            break;
         case 'Ana':
             echo 'Ola ' . $teste;
             echo 'Tudo ok?';
-            break; // Serve para parar o teste caso acima seja true
-        default: // Caso nenhuma das alternativas anteriores seja true
+            break;
+        default:
             echo 'Usuário incorreto!';
             echo 'Tente novamente!';
-    endswitch
+            break;
+    }
+
+    ?>
+    <hr>
+    <?php
+
+    $teste = 'Anna';
+
+    switch ($teste):
+
+        case 'Maria':
+            echo 'Ola ' . $teste;
+            echo 'Bem-vinda!';
+            break;
+        case 'Joca':
+            echo 'Ola ' . $teste;
+            echo 'Como vai!';
+            break;
+        case 'Ana':
+            echo 'Ola ' . $teste;
+            echo 'Tudo ok?';
+            break;
+        default:
+            echo 'Usuário incorreto!';
+            echo 'Tente novamente!';
+            break;
+
+    endswitch;
+
     ?>
 
 </body>
